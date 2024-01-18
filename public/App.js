52951f0cb0f962a2f6ed60b2b7265189c301cc1b
Zhifu Ge <zhifu@me.com>
2024-01-18 17:15:14 -0500

    03-dynamic-composition

--- public/App.js
+++ public/App.js
@@ -40,21 +40,33 @@ var IssueTable = /*#__PURE__*/function (_React$Component2) {
         border: "1px solid silver",
         padding: 4
       };
+      var issues = [{
+        id: 1,
+        status: 'New',
+        owner: 'Ravan',
+        effort: 5,
+        created: new Date('2018-08-15'),
+        due: undefined,
+        title: 'Error in console when clikcing Add'
+      }, {
+        id: 2,
+        status: 'Assigned',
+        owner: 'Eddie',
+        effort: 14,
+        created: new Date('2018-08-16'),
+        due: new Date('2018-08-30'),
+        title: 'Missing bottom border on panel'
+      }];
+      var issueRows = issues.map(function (issue) {
+        return /*#__PURE__*/React.createElement(IssueRow, {
+          key: issue.id,
+          rowStyle: rowStyle,
+          issue: issue
+        });
+      });
       return /*#__PURE__*/React.createElement("table", {
-        style: {
-          borderCollapse: "collapse"
-        }
-      }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
-        style: rowStyle
-      }, "ID"), /*#__PURE__*/React.createElement("th", {
-        style: rowStyle
-      }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
-        rowStyle: rowStyle,
-        issue_id: 1
-      }, "Error in console when clikcing Add"), /*#__PURE__*/React.createElement(IssueRow, {
-        rowStyle: rowStyle,
-        issue_id: 2
-      }, "Missing ", /*#__PURE__*/React.createElement("b", null, "bottom"), " border on panel")));
+        className: "bordered-table"
+      }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Created"), /*#__PURE__*/React.createElement("th", null, "Effort"), /*#__PURE__*/React.createElement("th", null, "Due Date"), /*#__PURE__*/React.createElement("th", null, "Title"))), /*#__PURE__*/React.createElement("tbody", null, issueRows));
     }
   }]);
   return IssueTable;
@@ -69,11 +81,8 @@ var IssueRow = /*#__PURE__*/function (_React$Component3) {
     key: "render",
     value: function render() {
       var style = this.props.rowStyle;
-      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
-        style: style
-      }, this.props.issue_id), /*#__PURE__*/React.createElement("td", {
-        style: style
-      }, this.props.children));
+      var issue = this.props.issue;
+      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, issue.id), /*#__PURE__*/React.createElement("td", null, issue.status), /*#__PURE__*/React.createElement("td", null, issue.owner), /*#__PURE__*/React.createElement("td", null, issue.created.toDateString()), /*#__PURE__*/React.createElement("td", null, issue.effort), /*#__PURE__*/React.createElement("td", null, issue.due ? issue.due.toDateString() : 'TBD'), /*#__PURE__*/React.createElement("td", null, issue.title));
     }
   }]);
   return IssueRow;

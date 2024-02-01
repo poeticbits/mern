1fef976b2e3fd0f9d2369424d4d096592d51b681
Zhifu Ge <zhifu@me.com>
2024-02-01 17:09:37 -0500

    05_the_about_api

--- server/server.js
+++ server/server.js
@@ -1,7 +1,37 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 
+let aboutMessage = "Issue Tracker API v1.0";
+
+const typeDefs = `
+    type Query {
+        about: String!
+    }
+    type Mutation {
+        setAboutMessage(message: String!): Boolean
+    }
+`;
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage,
+    },
+};
+
+function setAboutMessage(_, { message }) {
+    aboutMessage = message;
+    return true;
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
 const app = express();
-
+server.applyMiddleware({ app, path: '/graphql' });
 app.use(express.static('public'));
 
 app.listen(3000, function () {

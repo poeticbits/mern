b8898dd4b1e42461740680d4f383e9b9ddaa14fc
Zhifu Ge <zhifu@me.com>
2024-02-08 23:46:00 +0000

    05_the_create_api

--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@ const fs = require('fs');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
 
 const GraphQLDate = new GraphQLScalarType({
     name: 'GraphQLDate',
     description: 'A Date() type in GraphQL as a scalar',
     serialize(value) {
         return value.toISOString();
+    },
+    parseValue(value) {
+        return new Date(value);
+    },
+    parseLiteral(ast) {
+        return (ast.kind == Kind.STRING) ? new Date(ast.value) : undefined;
     },
 });
 
@@ -42,6 +49,7 @@ const resolvers = {
     },
     Mutation: {
         setAboutMessage,
+        issueAdd,
     },
     GraphQLDate,
 };
@@ -53,6 +61,16 @@ function issueList() {
 function setAboutMessage(_, { message }) {
     aboutMessage = message;
     return true;
+}
+
+function issueAdd(_, { issue }) {
+    issue.created = new Date();
+    issue.id = issuesDB.length + 1;
+
+    if (issue.status == undefined) issue.status = 'New';
+
+    issuesDB.push(issue);
+    return issue;
 }
 
 const server = new ApolloServer({

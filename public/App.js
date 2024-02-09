5eb2ebb6b39695892cd693ba14af211e531f87be
Zhifu Ge <zhifu@me.com>
2024-02-09 00:28:42 +0000

    05_query_variables

--- public/App.js
+++ public/App.js
@@ -185,7 +185,7 @@ var IssueList = /*#__PURE__*/function (_React$Component5) {
         return _regeneratorRuntime().wrap(function _callee2$(_context2) {
           while (1) switch (_context2.prev = _context2.next) {
             case 0:
-              query = "mutation {\n        issueAdd(issue: {\n            title: \"".concat(issue.title, "\",\n            owner: \"").concat(issue.owner, "\",\n            due: \"").concat(issue.due.toISOString(), "\",\n        }) {\n            id\n        }\n        }");
+              query = "mutation issueAdd($issue: IssueInputs!) {\n            issueAdd(issue:  $issue) {\n                id\n            }\n        }";
               _context2.next = 3;
               return fetch('/graphql', {
                 method: 'POST',
@@ -193,7 +193,10 @@ var IssueList = /*#__PURE__*/function (_React$Component5) {
                   'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                  query: query
+                  query: query,
+                  variables: {
+                    issue: issue
+                  }
                 })
               });
             case 3:

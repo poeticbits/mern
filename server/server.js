18beb9381eecabac926d1baa6470e2a8e495cae2
poeticbits <59710840+poeticbits@users.noreply.github.com>
2024-02-15 20:25:19 +0000

    06_reading_from_mongodb

--- server/server.js
+++ server/server.js
@@ -3,68 +3,28 @@ const express = require('express');
 const { ApolloServer, UserInputError } = require('apollo-server-express');
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
-
-const GraphQLDate = new GraphQLScalarType({
-    name: 'GraphQLDate',
-    description: 'A Date() type in GraphQL as a scalar',
-    serialize(value) {
-        return value.toISOString();
-    },
-    parseValue(value) {
-        const dateValue = new Date(value);
-        return isNaN(dateValue) ? undefined : dateValue;
-    },
-    parseLiteral(ast) {
-        if (ast.kind == Kind.STRING) {
-            const dateValue = new Date(ast.value);
-            return isNaN(dateValue) ? undefined : dateValue;
-        }
-    },
-});
+const { MongoClient } = require('mongodb');
 
 let aboutMessage = "Issue Tracker API v1.0";
-const issuesDB = [
-    {
-        id: 1,
-        status: 'New',
-        owner: 'Ravan',
-        effort: 5,
-        created: new Date('2018-08-15'),
-        due: undefined,
-        title: 'Error in console when clikcing Add',
-    },
-    {
-        id: 2,
-        status: 'Assigned',
-        owner: 'Eddie',
-        effort: 14,
-        created: new Date('2018-08-16'),
-        due: new Date('2018-08-30'),
-        title: 'Missing bottom border on panel',
-    },
-];
-
-const typeDefs = fs.readFileSync('./server/schema.graphql', 'utf-8');
-
-const resolvers = {
-    Query: {
-        about: () => aboutMessage,
-        issueList,
-    },
-    Mutation: {
-        setAboutMessage,
-        issueAdd,
-    },
-    GraphQLDate,
-};
-
-function issueList() {
-    return issuesDB;
-}
 
 function setAboutMessage(_, { message }) {
     aboutMessage = message;
     return true;
+}
+
+const url = "mongodb+srv://poeticbits:TY5wy1Et38snidLu@cluster0.zblreui.mongodb.net/?retryWrites=true&w=majority";
+let db;
+
+async function issueList() {
+    const issues = await db.collection('issues').find({}).toArray();
+    return issues;
+}
+
+async function connectToDb() {
+    const client = new MongoClient(url, { useNewUrlParser: true });
+    await client.connect();
+    console.log('Connected to MongoDB at', url);
+    db = client.db();
 }
 
 function issueAdd(_, { issue }) {
@@ -91,6 +51,40 @@ function validateIssue(issue) {
     }
 }
 
+const app = express();
+
+const GraphQLDate = new GraphQLScalarType({
+    name: 'GraphQLDate',
+    description: 'A Date() type in GraphQL as a scalar',
+    serialize(value) {
+        return value.toISOString();
+    },
+    parseValue(value) {
+        const dateValue = new Date(value);
+        return isNaN(dateValue) ? undefined : dateValue;
+    },
+    parseLiteral(ast) {
+        if (ast.kind == Kind.STRING) {
+            const dateValue = new Date(ast.value);
+            return isNaN(dateValue) ? undefined : dateValue;
+        }
+    },
+});
+
+const typeDefs = fs.readFileSync('./server/schema.graphql', 'utf-8');
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+        issueList,
+    },
+    Mutation: {
+        setAboutMessage,
+        issueAdd,
+    },
+    GraphQLDate,
+};
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -99,10 +93,19 @@ const server = new ApolloServer({
         return error;
     },
 })
-const app = express();
+
 server.applyMiddleware({ app, path: '/graphql' });
+
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-    console.log('App started on port 3000');
-});
+
+(async function () {
+    try {
+        await connectToDb();
+        app.listen(3000, function () {
+            console.log('App started on port 3000');
+        });
+    } catch (err) {
+        console.log('ERROR:', err);
+    }
+})();

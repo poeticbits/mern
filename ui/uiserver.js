e611294a160f0a2129aaf3c9a04b7d711dd7bdcc
Zhifu Ge <zhifu@me.com>
2024-03-14 16:42:57 -0700

    09_browser_history_router

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const proxy = require('http-proxy-middleware');
+const path = require('path');
 
 const app = express();
 app.use(express.static('public'));
@@ -9,6 +10,10 @@ const apiProxyTarget = process.env.API_PROXY_TARGET;
 if (apiProxyTarget) {
   app.use('/graphql', proxy({ target: apiProxyTarget }));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
+});
 
 const port = process.env.UI_SERVER_PORT || 8000;
 app.listen(port, () => {
